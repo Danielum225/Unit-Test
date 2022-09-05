e01a111507acd451d408372b9205c4ddd8ef852a
Danielum225 <danielalvarezpalmeiro@gmail.com>
2022-09-05 17:48:02 +0000

    trabajo finalizado

--- app.js
+++ app.js
@@ -1,30 +1,11 @@
-// esta es mi función que suma dos números
-const sum = (a,b) => {
-    return a + b
+const fromDollarToYen = usd => {
+    return usd * 127.09;
+}
+const fromEuroToDollar = eur => {
+    return eur * 1.07;
+}
+const fromYenToPound = yen => {
+    return yen * 0.0063;
 }
 
-// solo un registro en consola para nosotros.
-console.log(sum(7,3))
-
-let oneEuroIs = {
-    "JPY": 127.9, // japan yen
-    "USD": 1.2, // us dollar
-    "GBP": 0.8, // british pound
-}
-
-const fromEuroToDollar = function(valueInEuro){
-    let valueInDollar = valueInEuro * oneEuroIs.USD;
-    return valueInDollar;
-}
-
-const fromDollarToYen = function(valueInDollar){
-    let valueInYen = valueInDollar * oneEuroIs.JPY;
-    return valueInYen;
-}
-
-const fromYenToPound = function(valueInYen){
-    let valueInPound = valueInYen * oneEuroIs.GBP;
-    return valueInPound;
-}
-
-module.exports = {  fromEuroToDollar, fromDollarToYen, fromYenToPound };
+module.exports = {fromDollarToYen, fromEuroToDollar, fromYenToPound}
--- test.js
+++ test.js
@@ -1,35 +1,11 @@
-const { sum } = require('./app.js');
-
-test('adds 14 + 9 to equal 23', () => {
-    //dentro de la prueba llamamos a nuestra función sum con 2 números
-    let total = sum(14, 9);
-
-    // esperamos que la suma de esos 2 números sea 23
-    expect(total).toBe(23);
-});
-
-test("One euro should be 1.206 dollars", function(){
-    
-    const { fromEuroToDollar } = require('./app.js')
-    
-    const dollars = fromEuroToDollar(3.5)
-    
-    const expected = 3.5 * 1.2;
-    
-    expect(expected).toBe(dollars);
+test("One euro should be 1.07 dollars", function(){
+    expect(fromEuroToDollar(1)).toBe(1.07);
 })
-// DollarToYen
-test("One dollar should be 106.58 yens", function(){
-    const { fromDollarToYen } = require('./app.js')
-    const yens = fromDollarToYen(2.5)
-    const expected = 2.5 * 127.9;
-    expect(expected).toBe(yens);
+test("One dollar should be 127.09 yen", function(){
+    expect(fromDollarToYen(1)).toBe(127.09);
 })
-// YenToPound
-test("One pond should be 0.0062", function(){
-    const { fromYenToPound } = require('./app.js')
-    const pounds = fromYenToPound(3.5)
-    const expected = 3.5 * 0.8;
+test("One yen should be 0.0063 pounds", function(){
+    expect(fromYenToPound(1)).toBe(0.0063);
+})
 
-    expect(expected).toBe(pounds);
-})
+const {fromDollarToYen, fromEuroToDollar, fromYenToPound} = require('./app.js')
